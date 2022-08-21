c7e0ae2d581bbf3d788f5799ba749ab4c2057a26
Oleksandr Polishchuk <opolishchu@softserveinc.com>
2022-08-21 19:35:04 +0300

    feat(auth): add actions

--- src/app/enttities/auth/store/auth.actions.ts
+++ src/app/enttities/auth/store/auth.actions.ts
@@ -1,5 +1,7 @@
 import { createAction } from '@ngrx/store';
+import { AuthType } from './auth.enums';
 
-export const login = createAction('[Auth] Login');
-export const logout = createAction('[Auth] Logout');
-export const getUser = createAction('[Auth] Get User');
+export const login = createAction(AuthType.login);
+export const logout = createAction(AuthType.logout);
+export const setUser = createAction(AuthType.setUser);
+export const getUser = createAction(AuthType.getUser);
---
+++ src/app/enttities/auth/store/auth.enums.ts
@@ -0,0 +1,6 @@
+export enum AuthType {
+  login = '[Auth] Login',
+  logout = '[Auth] Logout',
+  setUser = '[Auth] Set User',
+  getUser = '[Auth] Get User',
+}
