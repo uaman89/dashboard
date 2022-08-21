6c3bde790618237e2ee280b8e7beaebbdf4cb793
Oleksandr Polishchuk <opolishchu@softserveinc.com>
2022-08-21 19:23:15 +0300

    ADD MISSED PAGE

---
+++ src/app/pages/dashboard/tasks-page/tasks-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TasksPageComponent } from './tasks-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    TasksPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TasksPageModule { }
