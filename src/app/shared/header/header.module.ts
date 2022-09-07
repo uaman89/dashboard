9083c64a536359ba7a6804a1f0793c3c430cb920
Oleksandr Polishchuk <opolishchu@softserveinc.com>
2022-09-07 16:20:52 +0300

    fix: add missed module

--- src/app/shared/header/header.module.ts
+++ src/app/shared/header/header.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatButtonModule } from "@angular/material/button";
+import { HeaderComponent } from './header.component';
 
 
 @NgModule({
@@ -13,7 +13,8 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [
     CommonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
   ],
   exports: [
     HeaderComponent
